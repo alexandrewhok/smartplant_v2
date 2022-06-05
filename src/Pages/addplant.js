b4a26e18e9d2250abe12f61a8923743551ce26c7
Alexandre Lucas <alexandrelucas@Alexandres-MacBook-Pro-3.local>
2022-06-05 17:01:31 +0100

    connections corrected and added dropdown

--- src/Pages/addplant.js
+++ src/Pages/addplant.js
@@ -1,10 +1,18 @@
+import Dropdown from 'react-dropdown';
+import 'react-dropdown/style.css';
+
 
 
 function addplant() {
+  const options = [
+     'Cacto', 'Orquídea', 'Monstera'
+  ];
+  const defaultOption = options[0];
+
     return (
       <div className="addplant">
         <div className="addplant-return">
-        <a href="/">
+        <a href="/home">
            <img  alt="back" src="./images/arrow.png"
            width="31" height="39"/>
         </a>
@@ -14,8 +22,11 @@ function addplant() {
               <div className="addplant-info-first text-label">Nome da planta</div>    
             <input className="addplant-info-field" type="text" placeholder="ex:Cacto" />             
         </div>
-        
-
+        <div className='addplant-select text-label'>Selecione o tipo de planta</div>
+        <div className="addplant-drop text-label"> 
+<Dropdown options={options} value={defaultOption} placeholder="Select an option" />
+</div>
+    <a  href="/home" className="button text-label">Adicionar Planta</a>
        </div>
     );
   }
